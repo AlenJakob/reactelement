302e246de1f0de30adcb80e109f1465f8abd97b7
Alen <grafuxa@gmail.com>
2023-01-15 20:41:59 +0100

    add constant modal config mock

--- src/helpers/consts.ts
+++ src/helpers/consts.ts
@@ -1,5 +1,12 @@
-export const SIZE_CONVERTED: any = {
+const SIZE_CONVERTED: any = {
 	small: 1,
 	medium: 2,
 	big: 3,
 };
+
+const modalConfigMock = {
+	title: 'Contrary to popular',
+	content:
+		'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock',
+};
+export { SIZE_CONVERTED, modalConfigMock };
