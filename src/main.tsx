8ca477b1c27fad1cea3550352d00aa65973af2a3
Alen <grafuxa@gmail.com>
2023-01-16 06:46:32 +0100

    expot base components for package usage

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import Modal from './components/modal/Modal';
+import NtModal from './components/modal/NtModal';
 import { NtButton } from './components/Button/NtButton';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import React from 'react';
+import NtIcon from './components/icon/NtIcon';
 
 // Components exported
 // Main
-export { Modal, NtButton };
+export { NtModal, NtButton, NtIcon };
 
-// Local tests:
+// Local tests file:
 
 createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
